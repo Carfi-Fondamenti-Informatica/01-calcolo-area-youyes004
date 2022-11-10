96dd9bc19faac20cf1ba5be63de0acad447d9ed1
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-11-10 14:44:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
+float a, b, c ;
+    cin >> a >> b >> c;
 //Qui va scritto il codice per eseguire i calcoli richiesti
+cout<<(a*b)/2<<endl;
+cout<<a*a<<endl;
+cout<<a*b<<endl;
+cout<<((a+b)*c)/2<<endl;
 
-
-}
